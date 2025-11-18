407989983e8326381d6df7c917d7409d57f215d4
Mateusz Ciurzyński <01187525@pw.edu.pl>
2025-11-18 21:09:49 +0100

    Added navbar

--- projekt/app/routes/main.py
+++ projekt/app/routes/main.py
@@ -1,13 +1,15 @@
 from flask import Blueprint, render_template
 
-from flask_login import login_required, current_user
+from flask_login import current_user
+from app.db import db, MembershipType
 
 main_bp = Blueprint('main', __name__, url_prefix='/')
 
 @main_bp.route('')
 def index():
     if not current_user.is_authenticated:
-        return render_template('main/default.html')
+        mem_types = db.session.execute(db.select(MembershipType).where(MembershipType.active == True)).scalars().all()
+        return render_template('main/default.html', mem_types=mem_types)
     if current_user.role == 'employee':
         return render_template('main/employee.html')
     if current_user.role == 'trainer':
